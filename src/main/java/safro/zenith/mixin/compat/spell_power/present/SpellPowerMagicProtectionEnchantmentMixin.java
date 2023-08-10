2a52f4777f52b2252699ee4f94f9dec8eaf5f7b1
TheWinABagel <gogglebobby@gmail.com>
2023-08-09 21:39:56 -0500

    magic protection support for prot changes, add stoneshelf recipe

---
+++ src/main/java/safro/zenith/mixin/compat/spell_power/present/SpellPowerMagicProtectionEnchantmentMixin.java
@@ -0,0 +1,35 @@
+package safro.zenith.mixin.compat.spell_power.present;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.ProtectionEnchantment;
+import net.spell_power.internals.MagicProtectionEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Fixes combining tools in an anvil reducing enchantment level to it's max level when the
+ * level of the enchantment is higher than it's max level. For example, combining level five
+ * efficiency pickaxe with a level six efficiency pickaxe (which is higher than the max) will
+ * keep the enchantment at level six instead of changing to it's max level of five.
+ * <p>
+ * Slightly edited to fix higher level enchantment anvil combining
+ * Author: MrCrayfish
+ */
+@Mixin(value = MagicProtectionEnchantment.class)
+public class SpellPowerMagicProtectionEnchantmentMixin extends ProtectionEnchantment
+{
+    public SpellPowerMagicProtectionEnchantmentMixin(Rarity rarity, Type type, EquipmentSlot... equipmentSlots) {
+        super(rarity, type, equipmentSlots);
+    }
+
+@Override
+    public boolean checkCompatibility(Enchantment ench) {
+        if (ench instanceof ProtectionEnchantment pEnch) {
+            return (pEnch.type == Type.ALL || pEnch.type == Type.FALL);
+        }
+        return (super.checkCompatibility(ench));
+    }
+}
