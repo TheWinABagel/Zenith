697366938dfa0bcc78964f129d9b8db1488f224f
TheWinABagel <gogglebobby@gmail.com>
2024-02-28 16:12:58 -0600

    properly check if anvil is vanilla

--- src/main/java/dev/shadowsoffire/apotheosis/ench/anvil/SplittingEnchant.java
+++ src/main/java/dev/shadowsoffire/apotheosis/ench/anvil/SplittingEnchant.java
@@ -2,7 +2,6 @@ package dev.shadowsoffire.apotheosis.ench.anvil;
 
 import dev.shadowsoffire.apotheosis.ench.EnchModule;
 import io.github.fabricators_of_create.porting_lib.enchant.CustomEnchantingTableBehaviorEnchantment;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
@@ -15,7 +14,7 @@ public class SplittingEnchant extends Enchantment implements CustomEnchantingTab
 
     @Override
     public boolean canApplyAtEnchantingTable(ItemStack stack) {
-        return CustomEnchantingTableBehaviorEnchantment.super.canApplyAtEnchantingTable(stack) || stack.is(ItemTags.ANVIL);
+        return CustomEnchantingTableBehaviorEnchantment.super.canApplyAtEnchantingTable(stack) || EnchModule.isVanillaAnvil(stack);
     }
 
     @Override
